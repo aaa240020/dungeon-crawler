ea635d84e97066fec8c9761376e70f1779dda543
Alan Alvarado <alanalvara@hotmail.com>
2025-05-14 18:24:58 -0500

    Added a new object called world that makes establishes a map and two sprite groups, normal visable sprites and collisions (obstacles)

--- src/project.py
+++ src/project.py
@@ -1,4 +1,32 @@
 import pygame
+from map import *
+
+class World:
+
+    def __init__(self):
+        
+        self.surface = pygame.display.get_surface()
+        
+        self.sprites = pygame.sprite.Group()
+        self.obstacles = pygame.sprite.Group()
+        
+        self.map()
+
+    def map(self):
+        for rindex,row in enumerate(FACE_MAP):
+            for cindex, col in enumerate(row):
+                x = cindex * tile_size
+                y = rindex * tile_size
+                if col == 'x':
+                    SpriteTexture((x,y),[self.sprites])
+                if col == '0':
+    
+    def main(self):
+
+        self.sprites.draw(self.surface)
+
+
+#class Main_Character:
 
 class Crawler:
 
@@ -12,6 +40,8 @@ class Crawler:
         self.screen = pygame.display.set_mode((self.resolution))
         pygame.display.set_caption("Crawler")
         self.time = pygame.time.Clock()
+
+        self.world = World()
 
     def main(self):
 
@@ -32,7 +62,7 @@ class Crawler:
             color = pygame.Color(0, 255, 255)
             self.screen.fill(color)
             pygame.display.update()
-            self.time.tick(24)
+            self.time.tick(60)
 
         pygame.quit()
 
