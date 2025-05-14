30a7ca1ab1d6c8124fc1a25dabee8526a6b37cbf
Alan Alvarado <alanalvara@hotmail.com>
2025-05-14 14:55:47 -0500

    Changed the code around. (Still doesn't work)

--- src/project.py
+++ src/project.py
@@ -4,34 +4,33 @@ class Crawler:
 
     def __init__(self):
 
-        resolution = (800,600)
         pygame.init()
-        self.screen = pygame.display.set_mode((resolution))
+        self.screen = pygame.display.set_mode((self.resolution))
+        pygame.display.set_caption("Crawler")
         self.time = pygame.time.Clock()
 
     def main(self):
 
-        resolution = (800,600)
         running = True
-        fullscreen = 1
+        self.resolution = (800,600)
+        self.fullscreen_resolution = (1920,1080)
+        self.fullscreen = False
 
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-                    pygame.quit()
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_F11:
-                        fullscreen += 1
-                    if fullscreen % 2 == 0:
-                        fullscreen_resolution = (1920, 1080)
-                        screen = pygame.display.set_mode(fullscreen_resolution, pygame.FULLSCREEN)
-                        Crawler(fullscreen_resolution)
-                    else:
-                        screen = pygame.display.set_mode(resolution)
-                        Crawler(resolution)
+                        self.fullscreen = not self.fullscreen
+                        if self.fullscreen:
+                            self.screen = pygame.display.set_mode(self.fullscreen_resolution, pygame.FULLSCREEN)
+                        else:
+                            self.screen = pygame.display.set_mode(self.resolution)
 
-        black = pygame.Color(0, 0, 0)
-        screen.fill(black)
-        pygame.display.update()
-        self.clock.tick(24)
+            color = pygame.Color(0, 255, 255)
+            self.screen.fill(color)
+            pygame.display.update()
+            self.clock.tick(24)
+
+        pygame.quit()
